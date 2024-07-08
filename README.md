ce0184077e01c3b1775cf7e685e7d796b1f8a067
Rivio98 <rivaldogjoni1998@gmail.com>
2024-07-08 17:15:35 +0200

    [UPD] Created readme file

---
+++ README.md
@@ -0,0 +1,7 @@
+1 creo una variabile per la posizione delle bombe
+
+2 creo una funzione con dentro un for per ciclare in maniera random le bombe
+
+3 inserisco le bombe dentro le caselle generate in precedenza.
+-se l'utente preme una casella contenente una bomba il gioco finisce ed apparirà un messaggio di fine gioco
+-altrimenti si continua
---
+++ js/script.js
@@ -0,0 +1,41 @@
+//Creo una funzione per mettere dentro la grid un quadrato
+function createSingleSquare(num) {
+    //creo il quadrato come div
+    const square = document.createElement('div');
+    //aggiungo la classe square al div
+    square.classList.add('square');
+    //aggiungo il numero dentro il div square
+    square.innerText = num + 1;
+
+    //Aggiungo l'evento click al quadrato
+    square.addEventListener('click', function () {
+        this.classList.add('clicked');
+
+    });
+
+    //restituisco il quadrato
+    return square;
+}
+
+//Recupero il pulsante dall HTML
+const button = document.getElementsByName('play');
+
+//Aggiungo l'evento
+play.addEventListener('click', function () {
+
+    //Recupero l'elemento che conterrà la griglia
+    const grid = document.getElementById('grid');
+
+    //aggiungo il 'reset' al pulsante gioca in modo che resetti il tutto
+    grid.innerText = '';
+    //ciclo per creare 100 caselle 
+    for (let i = 0; i < 100; i++) {
+        //chiamo la funzione per creare le caselle
+        let item = createSingleSquare(i);
+        //aggiungo la casella alla griglia
+        grid.append(item);
+    }
+});
+
+
+
